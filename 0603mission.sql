5bedcb9688cdba0adfc9b4a006215698243b69e1
Juyoung Chung <84014277+dilatata@users.noreply.github.com>
2021-06-03 16:06:16 +0900

    Update 0603mission.sql
    line edit

--- 0603mission.sql
+++ 0603mission.sql
@@ -1,12 +1,12 @@
 -- table 1 celebrity
 
 create table celebrity(
-    cl_id number, --pk
+	cl_id number, --pk
 	cl_name varchar2(10) NOT NULL,
-	birth date ,
-    agency varchar2(100), 
-    job varchar2(100) , 
-    constraint pk_celebrity_cl_id primary key(cl_id)
+	birth date,
+	agency varchar2(100),
+	job varchar2(100), 
+	constraint pk_celebrity_cl_id primary key(cl_id)
 );
 
 
@@ -14,32 +14,32 @@ create table celebrity(
 
 -- table 2 programs
 create table programs(
-    pr_id number, --pk
+	pr_id number, --pk
 	pr_name varchar2(100) NOT NULL,
-    start_date date ,
-    end_date date,
-    producer varchar2(10) ,
-    br_id number NOT NULL, --fk
-    constraint pk_programs_pr_id primary key(pr_id),
-    constraint fk_programs_br_id foreign key (br_id) references broadcast(br_id)
+	start_date date,
+	end_date date,
+	producer varchar2(10),
+	br_id number NOT NULL, --fk
+	constraint pk_programs_pr_id primary key(pr_id),
+	constraint fk_programs_br_id foreign key (br_id) references broadcast(br_id)
 );
 
 -- table 3 broadcast
 create table broadcast(
 	br_id number, --pk
-    br_name varchar2(10) NOT NULL,
-    loc varchar2(100),
-    constraint pk_broadcast_br_id primary key(br_id)
+	br_name varchar2(10) NOT NULL,
+	loc varchar2(100),
+	constraint pk_broadcast_br_id primary key(br_id)
 );
 
 
 
 -- table 4 celebrity_programs_match
 create table cl_br_match(
-    cl_id number , --fk
-    pr_id number , --fk
-    constraint fk_cl_br_match_cl_id foreign key (cl_id) references celebrity(cl_id),
-    constraint fk_cl_br_match_pr_id foreign key (pr_id) references programs(pr_id)
+	cl_id number , --fk
+	pr_id number , --fk
+	constraint fk_cl_br_match_cl_id foreign key (cl_id) references celebrity(cl_id),
+	constraint fk_cl_br_match_pr_id foreign key (pr_id) references programs(pr_id)
 );
 
 
