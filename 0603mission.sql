07d09ccdbc6e2860ab84ef14eca3d4f255b6bfbc
zero_0_1103 <83809838+dlwlsdudo1@users.noreply.github.com>
2021-06-03 16:19:17 +0900

    programs update
    d

--- 0603mission.sql
+++ 0603mission.sql
@@ -64,8 +64,17 @@ insert into  celebrity (cl_id, cl_name, birth, agency, job) values (16, '김지�
 insert into  celebrity (cl_id, cl_name, birth, agency, job) values (17, '강신일', '1960/11/26', '스타빌리지엔터테인먼트', 'actor');
 
 --
-insert into  programs (pr_id, pr_name, start_date, producer, br_id) values (1, '무한도전', '2006/05/06', '2018/03/31',	'김태호', 2)
-
+insert into  programs (pr_id, pr_name, start_date, producer, br_id)
+values(1, '무한도전', '2006/05/06', '2018/03/31', '김태호', 2);
+values(2, '1박2일', '2007/08/05', NULL, '나영석', 1);
+values(3, '동물농장', '2001/05/01', NULL, '이덕건', 3);
+values(4, '런닝맨', '2010/07/11', NULL, '정철민', 3);
+values(5, '유희열의 스케치북',	'2004/24/09', 'NULL', '이황선', 1);
+values(6, '놀면 뭐하니', '2019/07/27', NULL, '김태호', 2);
+values(7, '호텔 델루나', '2019/07/13', '2019/09/01', '이동규', 5);
+values(8, '100분 토론', '1999/10/21', NULL, '김성진',	2);
+values(9, '빈센조', '2021/02/20', '2021/05/02', '조문주', 5);
+values(10, '태양의 후예', '2016/02/24', '2016/04/14', '배경수', 1);
 
 --
 insert into  broadcast (br_id, br_name, loc) values (1, 'KBS2', '서울시 영등포구')
